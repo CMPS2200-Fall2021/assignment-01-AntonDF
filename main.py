649d39a377f96d7939f68c6e0fe456e4045c11a3
AntonDF <89557531+AntonDF@users.noreply.github.com>
2021-10-01 20:53:33 -0500

    new

--- main.py
+++ main.py
@@ -3,13 +3,30 @@ CMPS 2200  Assignment 1.
 See assignment-01.pdf for details.
 """
 # no imports needed.
-
+"""gives xth number of the fibonnaci sequence"""
 def foo(x):
-    ### TODO
+    if x <= 1:
+        return x
+    else:
+        ra = foo(x-1)
+        rb = foo(x-2)
+        return ra+rb
     pass
 
 def longest_run(mylist, key):
-    ### TODO
+    counter = 0
+    max = 0
+    for i in mylist:
+        print(counter)
+        if i == key:
+            counter = counter + 1
+
+        else:
+            if counter > max:
+                max = counter
+            counter = 0
+
+    return max
     pass
 
 
@@ -27,11 +44,21 @@ class Result:
     
     
 def longest_run_recursive(mylist, key):
-    ### TODO
+    count = 0
+    max = 0
+    if len(mylist) == 1:
+        count = count + 1
+        return mylist[i]
+    else:
+        longest_run_recursive(mylist, key)
+
     pass
 
 ## Feel free to add your own tests here.
 def test_longest_run():
     assert longest_run([2,12,12,8,12,12,12,0,12,1], 12) == 3
+    assert longest_run([1,1,1,2,3,4,5,1,1,1,1,1,1,0], 1) == 6
 
+foo(9)
+test_longest_run()
 
